ab6e7dd9c564ef9979c74f550b30ae88cff6917e
silencemerchant <silencemerchant@gmail.com>
2025-12-04 20:51:03 +1300

    Delete server directory

--- server/autoRetrainingEngine_1.ts
+++
@@ -1,9 +0,0 @@
-// Placeholder for the autoRetrainingEngine
-
-class AutoRetrainingEngine {
-    addFeedback(feedback: any) {
-        console.log("Adding feedback to retraining engine (mocked)...");
-    }
-}
-
-export const autoRetrainingEngine = new AutoRetrainingEngine();
